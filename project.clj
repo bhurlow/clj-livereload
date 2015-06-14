c1d73849d53d2c5c330117da72805ccbe6ad56d1
Juho Teperi <juho.teperi@iki.fi>
2015-06-14 17:03:37 +0300

    Removed :main property from project.clj

--- project.clj
+++ project.clj
@@ -3,7 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :main clj-livereload.core
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [cheshire "5.4.0"]
                  [ring "1.3.1"]
