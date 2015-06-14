e7aa4acc5ed0ff890c40ed4d5034b7cce4c6b9ac
Juho Teperi <juho.teperi@iki.fi>
2015-06-14 19:23:36 +0300

    Add some options to control the output

--- src/clj_livereload/core.clj
+++ src/clj_livereload/core.clj
@@ -6,20 +6,21 @@
             [cheshire.core :as json]
             [hawk.core :as hawk]))
 
+(defn- info [state & args] (if-not (:silent? @state) (apply println args)))
+(defn- dbug [state & args] (if (:debug? @state) (apply println args)))
+
 (defn- hello-message []
   {:command "hello"
-   :protocols ["http://livereload.com/protocols/official-7"
-               "http://livereload.com/protocols/official-8"
-               "http://livereload.com/protocols/official-9"
-               "http://livereload.com/protocols/2.x-origin-version-negotiation"
-               "http://livereload.com/protocols/2.x-remote-control"]
+   ; Tiny-lr supports only this, so I think it's safe to do the same
+   :protocols ["http://livereload.com/protocols/official-7"]
    :serverName "clj-livereload"})
 
 (defn css-file? [path]
   (.endsWith path ".css"))
 
 (defn send-reload-msg [state path]
-  (println "trigggered reload for path" path)
+  (info state "Reloading:" path)
+  (dbug state "Sending changes to" (count (:reload-channels @state)) "clients")
   (doseq [channel (:reload-channels @state)]
     (if (open? channel)
       (send! channel
@@ -32,11 +33,10 @@
 (defn- watch-params [state paths]
   [{:paths [paths]
     :handler (fn [ctx e]
-               (println "detected file change")
                (send-reload-msg state (.getName (:file e))))}])
 
 (defn- watch-directory [state dir]
-  (println "starting to watch dir..")
+  (info state "Starting to watch dir..")
   (hawk/watch! (watch-params state dir)))
 
 (defn- handle-livereload [state req]
@@ -46,7 +46,7 @@
       channel
       (fn [data]
         (let [parsed (json/decode data true)]
-          (println parsed)
+          (dbug state "Received message:" parsed)
           (case (:command parsed)
             "hello" (send! channel (json/generate-string (hello-message)))
             nil))))
@@ -65,8 +65,11 @@
 
 (defn create-state
   "Creates the state holding open websocket connections."
-  []
-  (atom {:reload-channels #{}}))
+  [opts]
+  (atom (merge (select-keys opts [:debug? :silent?])
+               {:reload-channels #{}})))
+
+(def default-port 35729)
 
 (defn start
   "Starts the http-server and (by default) watch service.
@@ -75,13 +78,21 @@
 
    Options:
    - :port     Default 35729.
-   - :watch    Default true. To disable directory watching, set false."
+   - :watch    Default true. To disable directory watching, set false.
+   - :silent?  Disable output.
+   - :deblug?  Enable debug output."
   [{:keys [dir port watch]
-    :or {watch true}}]
-  (let [state (create-state)]
-    (println "starting LiveReload server")
+    :or {watch true}
+    :as opts}]
+  (let [port (or port default-port)
+        state (create-state opts)]
+    (when-not (= port default-port)
+      (println "Warn: LiveReload port is not standard (%s). You are listening on %d." default-port port)
+      (println "You'll need to rely on the LiveReload snipper")
+      (println "> http://feedback.livereload.com/knowledgebase/articles/86180-how-do-i-add-the-script-tag-manually-"))
+    (info state "Starting LiveReload server")
     {:state state
-     :http-kit (run-server (handler state) {:port (or port 35729)})
+     :http-kit (run-server (handler state) {:port port})
      :watch (if watch
               (watch-directory state dir))}))
 
