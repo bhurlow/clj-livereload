fe8c6fb296b2f3eaf654dff9f7a9b7b76fe427f5
Brian Hurlow <bhurlow@gmail.com>
2015-06-17 15:36:02 -0500

    version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-livereload "0.1.1"
+(defproject clj-livereload "0.2.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
